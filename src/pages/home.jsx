aa0fc74cfe1093f2659dc7dcad987121673f85ff
thianah <ekundayo6789@gmail.com>
2025-01-25 16:53:52 +0000

    Updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image from "../assets/image.jpg";
 //creating home page component= home
 function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className="home-container">
-      <h1 className="home-title">
+      <h1 className={`home-title ${isVisible ? "visible" : ""}`}>
         {" "}
         HELLO!
         <br />
